14857f841d135fe26f763b8d349411ea9a326cfe
John3814 <john.zapata1@udea.edu.co>
2023-07-28 10:19:53 -0500

    menu principal

--- src/com/conversor/Conversor.java
+++ src/com/conversor/Conversor.java
@@ -1,10 +1,37 @@
 package com.conversor;
 
+import javax.swing.JOptionPane;
+
 public class Conversor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		boolean bandera = false;
+		
+		do {
+			String opcion = menuPrincipal();
+			switch (opcion) {
+				case "Conversor de Moneda": 
+					break;
+				case "Conversor de Temperatura":
+					break;
+				case "Salir":
+					bandera = true;
+					break;
+			}
+		} while (!bandera);
+		
+	}
 
+	public static String menuPrincipal() {
+		Object[] opciones = {"Conversor de Moneda", "Conversor de Temperatura"};
+		
+		Object seleccion = JOptionPane.showInputDialog(null, "Seleccione una opción de conversión", 
+				"Menu", JOptionPane.PLAIN_MESSAGE, null, opciones, opciones[0]);
+		if(seleccion != null){		
+			return seleccion.toString();
+		}
+		return "Salir";
 	}
 
 }
