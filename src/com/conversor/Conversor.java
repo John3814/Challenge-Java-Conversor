b973dfa6141dac16c79d439419f118ec14cbc3a6
John3814 <john.zapata1@udea.edu.co>
2023-07-28 11:30:33 -0500

    Entrada de valor a convertir

--- src/com/conversor/Conversor.java
+++ src/com/conversor/Conversor.java
@@ -1,5 +1,8 @@
 package com.conversor;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.swing.JOptionPane;
 
 public class Conversor {
@@ -12,6 +15,7 @@ public class Conversor {
 			String opcion = menuPrincipal();
 			switch (opcion) {
 				case "Conversor de Moneda": 
+					Double valor = leerValor();
 					break;
 				case "Conversor de Temperatura":
 					break;
@@ -22,7 +26,35 @@ public class Conversor {
 		} while (!bandera);
 		
 	}
+	
 
+
+	private static Double leerValor() {
+		String entrada = JOptionPane.showInputDialog(null, 
+				"Ingresa la cantidad de dinero que deseas convertir:", "Input", JOptionPane.QUESTION_MESSAGE);
+		if(entrada != null) {
+			entrada = entrada.replace(".", "");
+			entrada = entrada.replace(",", ".");
+			if(validarEntrada(entrada)) {
+				return Double.parseDouble(entrada);
+			}else {
+				JOptionPane.showMessageDialog(null, "Valor no válido",
+						"Error", JOptionPane.INFORMATION_MESSAGE);
+				return leerValor();
+			}
+		}
+		return null;
+	}
+
+	
+	private static boolean validarEntrada(String entrada) {
+		String patronNumerico = "^[0-9]+(\\.[0-9]+)?$";
+		Pattern pattern = Pattern.compile(patronNumerico);
+		Matcher matcher = pattern.matcher(entrada);
+		return matcher.matches();
+	}
+
+	
 	public static String menuPrincipal() {
 		Object[] opciones = {"Conversor de Moneda", "Conversor de Temperatura"};
 		
